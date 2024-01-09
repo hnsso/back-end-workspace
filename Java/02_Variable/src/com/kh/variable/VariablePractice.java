512ffe23d3d33065dc500462a48fd5a1771e3217
hnsso <152463185+hnsso@users.noreply.github.com>
2024-01-09 17:50:31 +0900

    hnsso
    what

---
+++ Java/02_Variable/src/com/kh/variable/VariablePractice.java
@@ -0,0 +1,25 @@
+// 클래스 이름 : com.kh.practice.VariablePractice
+// FQCN(Fully Qualified Class Name)
+package com.kh.variable;
+
+public class VariablePractice {
+
+	public static void main(String[] args) {
+		VariablePractice v = new VariablePractice() ;
+		v.method1();
+	}
+	public void method1() {
+		
+		
+		int b = '10000';
+		int s = '7000';		
+		
+		System.out.print('성인 2명');
+		System.out.print();
+		
+		System.out.println( );
+		
+		System.out.print();
+		
+	}
+}
